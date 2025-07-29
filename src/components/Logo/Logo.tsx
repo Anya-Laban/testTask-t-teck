01403137efb96d41a7b19d21df2ecf674536867c
Anna Laban <laban.anya.a@gmail.com>
2025-07-29 14:30:03 +0300

    Update logo img path

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import s from "./Logo.module.css";
 export const Logo = () => {
   return (
     <a className={s.logo} href="/">
-      <img src="/src/assets/logo.png" alt="Titan logo" />
+      <img src="/public/logo.png" alt="Titan logo" />
     </a>
   );
 };
